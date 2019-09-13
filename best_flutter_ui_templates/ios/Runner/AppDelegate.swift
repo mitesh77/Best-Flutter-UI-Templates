4670eea75090bb28c37650e802d7b1f52e6ed61d
Sarthak Verma <hisarthakverma@gmail.com>
2019-09-13 23:06:34 +0530

    Revert "Fixed IOS Build Error on Flutter 1.7.8"
    This reverts commit bbe4c01f56e49d6597db05cc9d500444130b7dae.

--- best_flutter_ui_templates/ios/Runner/AppDelegate.swift
+++ best_flutter_ui_templates/ios/Runner/AppDelegate.swift
@@ -3,8 +3,10 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
-    -> Bool {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
