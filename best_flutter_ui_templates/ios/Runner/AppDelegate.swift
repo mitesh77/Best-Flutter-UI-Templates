bbe4c01f56e49d6597db05cc9d500444130b7dae
Sarthak Verma <hisarthakverma@gmail.com>
2019-09-13 23:02:05 +0530

    Fixed IOS Build Error on Flutter 1.7.8
    === BUILD TARGET Runner OF PROJECT Runner WITH CONFIGURATION Debug ===
      Best-Flutter-UI-Templates/best_flutter_ui_templates/ios/Runner/AppDelegate.swift:8:65: error: 'LaunchOptionsKey' is not a member type of 'UIApplication'
            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
                                                          ~~~~~~~~~~~~~ ^

--- best_flutter_ui_templates/ios/Runner/AppDelegate.swift
+++ best_flutter_ui_templates/ios/Runner/AppDelegate.swift
@@ -3,10 +3,8 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+    -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
