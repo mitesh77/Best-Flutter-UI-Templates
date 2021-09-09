b8725f4b866e99e6715914a546d1a97414fa4d61
nohli <43643339+nohli@users.noreply.github.com>
2021-09-09 09:49:02 +0200

    Update to Flutter 2.5, remove deprecations

--- lib/hotel_booking/hotel_app_theme.dart
+++ lib/hotel_booking/hotel_app_theme.dart
@@ -31,11 +31,9 @@ class HotelAppTheme {
     return base.copyWith(
       colorScheme: colorScheme,
       primaryColor: primaryColor,
-      buttonColor: primaryColor,
       indicatorColor: Colors.white,
       splashColor: Colors.white24,
       splashFactory: InkRipple.splashFactory,
-      accentColor: secondaryColor,
       canvasColor: Colors.white,
       backgroundColor: const Color(0xFFFFFFFF),
       scaffoldBackgroundColor: const Color(0xFFF6F6F6),
@@ -46,7 +44,6 @@ class HotelAppTheme {
       ),
       textTheme: _buildTextTheme(base.textTheme),
       primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
-      accentTextTheme: _buildTextTheme(base.accentTextTheme),
       platform: TargetPlatform.iOS,
     );
   }
