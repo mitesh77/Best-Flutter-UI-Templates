efb4df9c7a0c14bb1649b830b88aa15055a4f907
nohli <43643339+nohli@users.noreply.github.com>
2021-12-09 13:31:55 +0100

    Remove unnecessary imports

--- lib/hotel_booking/calendar_popup_view.dart
+++ lib/hotel_booking/calendar_popup_view.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
--- lib/hotel_booking/hotel_home_screen.dart
+++ lib/hotel_booking/hotel_home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
