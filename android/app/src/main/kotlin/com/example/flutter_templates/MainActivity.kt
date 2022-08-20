378127cf987f1bd8d3b23161ff78a57eee316e83
nohli <43643339+nohli@users.noreply.github.com>
2022-08-20 19:35:06 +0200

    Change name to templates, shrink images

--- android/app/src/main/kotlin/com/example/flutter_templates/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_templates
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,12 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "best_flutter_ui_templates");
+    gtk_header_bar_set_title(header_bar, "Templates");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  }
-  else {
-    gtk_window_set_title(window, "best_flutter_ui_templates");
+  } else {
+    gtk_window_set_title(window, "Templates");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
